5cebee704ba04abedbca526c0d4bd62639ae7d6e
jamesfinn1212 <james.finn2@ucdconnect.ie>
2024-02-07 13:32:09 +0000

    First push

---
+++ src/Draw.java
@@ -0,0 +1,67 @@
+public class Draw {
+    public static void drawBoard(){
+
+        System.out.println("                             / \\         / \\         / \\         / \\         / \\");
+        System.out.println("                           /     \\     /     \\     /     \\     /     \\     /     \\");
+        System.out.println("                         /         \\ /         \\ /         \\ /         \\ /         \\");
+        System.out.println("                        |           |           |           |           |           |");
+        System.out.println("                        |     .     |     .     |     .     |      .    |           |");
+        System.out.println("                        |           |           |           |           |           |");
+        System.out.println("                       / \\         / \\         / \\         / \\         / \\         / \\");
+        System.out.println("                     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\");
+        System.out.println("                   /         \\ /         \\ /         \\ /         \\ /         \\ /         \\");
+        System.out.println("                  |           |           |           |           |           |           |");
+        System.out.println("                  |           |           |           |           |           |           |");
+        System.out.println("                  |           |           |           |           |           |           |");
+        System.out.println("                 / \\         / \\         / \\         / \\         / \\         / \\         / \\");
+        System.out.println("               /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\");
+        System.out.println("             /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\");
+        System.out.println("            |           |           |           |           |           |           |           |");
+        System.out.println("            |           |           |           |           |           |           |           |");
+        System.out.println("            |           |           |           |           |           |           |           |");
+        System.out.println("           / \\         / \\         / \\         / \\         / \\         / \\         / \\         / \\");
+        System.out.println("         /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\");
+        System.out.println("       /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\");
+        System.out.println("      |           |           |           |           |           |           |           |           |");
+        System.out.println("      |           |           |           |           |           |           |           |           |");
+        System.out.println("      |           |           |           |           |           |           |           |           |");
+        System.out.println("     / \\         / \\         / \\         / \\         / \\         / \\         / \\         / \\         / \\");
+        System.out.println("   /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\");
+        System.out.println(" /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\");
+        System.out.println("|           |           |           |           |           |           |           |           |           |");
+        System.out.println("|          |           |           |           |           |           |           |           |           |");
+        System.out.println("|           |           |           |           |           |           |           |           |           |");
+        System.out.println(" \\         / \\         / \\         / \\         / \\         / \\         / \\         / \\         / \\         /");
+        System.out.println("   \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /");
+        System.out.println("     \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /");
+        System.out.println("      |           |           |           |           |           |           |           |           |");
+        System.out.println("      |     0     |           |           |           |           |           |           |           |");
+        System.out.println("      |           |           |           |           |           |           |           |           |");
+        System.out.println("       \\         / \\         / \\         / \\         / \\         / \\         / \\         / \\         /");
+        System.out.println("         \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /");
+        System.out.println("           \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /");
+        System.out.println("            |           |           |           |           |           |           |           |");
+        System.out.println("            |     0     |           |           |           |           |           |           |");
+        System.out.println("            |           |           |           |           |           |           |           |");
+        System.out.println("             \\         / \\         / \\         / \\         / \\         / \\         / \\         /");
+        System.out.println("               \\     /     \\     /     \\     /     \\     /     \\     /     \\     /     \\     /");
+        System.out.println("                 \\ /         \\ /         \\ /         \\ /         \\ /         \\ /         \\ /");
+        System.out.println("                  |           |           |           |           |           |           |");
+        System.out.println("                  |           |           |           |           |           |           |");
+        System.out.println("                  |           |           |           |           |           |           |");
+        System.out.println("                  \\         / \\         / \\         / \\         / \\         / \\         /");
+        System.out.println("                    \\     /     \\     /     \\     /     \\     /     \\     /     \\     /");
+        System.out.println("                      \\ /         \\ /         \\ /         \\ /         \\ /         \\ /");
+        System.out.println("                       |           |           |           |           |           |");
+        System.out.println("                       |           |           |           |           |           |");
+        System.out.println("                       |           |           |           |           |           |");
+        System.out.println("                        \\         / \\         / \\         / \\         / \\         /");
+        System.out.println("                          \\     /     \\     /     \\     /     \\     /     \\     /");
+        System.out.println("                            \\ /         \\ /         \\ /         \\ /         \\ /");
+
+
+
+    }
+
+
+}
